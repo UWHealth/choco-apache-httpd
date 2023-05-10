5bc88c099f14903cc7b3c9a67f6c9f0deda03de1
Nathan Morris <nmorris@uwhealth.org>
2023-05-10 14:40:13 -0500

    fix:Update update.ps1 increment versionPostfix

--- apache-httpd/update.ps1
+++ apache-httpd/update.ps1
@@ -5,7 +5,7 @@
 # $releases = 'https://www.apachehaus.com/cgi-bin/download.plx'
 $releases = 'https://www.apachelounge.com/download/'
 
-$versionPostfix = "0000"
+$versionPostfix = "0001"
 
 $global:au_NoCheckChocoVersion = $true
 
